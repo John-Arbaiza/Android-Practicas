5a749ed1bd5832f49b938db22f4123ef0a86a1b6
JohnArbaiza1 <john.arbaiza0709@gmail.com>
2024-02-25 23:04:58 -0600

    Segundo proyecto actualizado

---
+++ Invitacion/app/src/main/java/com/example/invitacion/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.invitacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Definimos una variable con el tipo de control
+    Button btnSiguiente;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Parte encargada del funcionamiento de los buttons
+
+        //capturamos el id
+        btnSiguiente = findViewById(R.id.btnInicio);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            //Evento click
+            @Override
+            public void onClick(View v) {
+                //Especificamos a la actividad que queremos ir
+                Intent intent = new Intent(MainActivity.this, invitacion.class);
+                startActivity(intent);
+
+
+            }
+        });
+    }
+
+
+
+
+}
