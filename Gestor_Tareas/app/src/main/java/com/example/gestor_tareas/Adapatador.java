50fcc23752a8d29131effb0d55ed40dfff3d4ef9
JohnArbaiza1 <john.arbaiza0709@gmail.com>
2024-03-28 16:22:44 -0600

    Actualizacion de gestor de tareas

--- Gestor_Tareas/app/src/main/java/com/example/gestor_tareas/Adapatador.java
+++ Gestor_Tareas/app/src/main/java/com/example/gestor_tareas/Adapatador.java
@@ -1,11 +1,15 @@
 package com.example.gestor_tareas;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -52,13 +56,51 @@ public class Adapatador  extends BaseAdapter {
         TextView txt1 = vista.findViewById(R.id.textView5);
         TextView txt2 = vista.findViewById(R.id.textView6);
         TextView txt3 = vista.findViewById(R.id.textView7);
+        ImageButton btnEliminar = vista.findViewById(R.id.imageButton);
 
         txt1.setText(itemTarea.getTitle());
         txt2.setText(itemTarea.getPrioridad());
         txt3.setText(itemTarea.getCategoria());
+
+        //Definimos el evento del button  encargado de eliminar
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Instanciamos un objeto de tipo AlertDialog
+                AlertDialog alerta = alertaEliminar(position);
+                alerta.show();
+
+            }
+        });
+
         //retornamos la vista
         return vista;
+
     }
+
+    //Definimos un AlertDialog
+    public AlertDialog alertaEliminar(int position){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context).setTitle("Eliminar Tarea").setMessage("Seguro que desea eliminar esta tarea")
+                .setPositiveButton("si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        categoriaTArea.remove(position);
+                        notifyDataSetChanged();
+                        
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "Tarea no eliminada", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        return builder.create();
+    }
+
 }
+
+
 
 
